89b65726ebb296d0641be954f97668b9ba181ea9
Matt Mikula <3180277+mattmikula@users.noreply.github.com>
2025-10-20 20:12:13 -0400

    Add profile editing UI and backend support

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -2,10 +2,12 @@ from .comment_service import CommentService
 from .favorite_service import FavoriteService
 from .rating_service import RatingService
 from .skate_spot_service import SkateSpotService
+from .user_profile_service import UserProfileService
 
 __all__ = [
     "SkateSpotService",
     "RatingService",
     "FavoriteService",
     "CommentService",
+    "UserProfileService",
 ]
